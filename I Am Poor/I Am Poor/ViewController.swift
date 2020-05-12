36453f985be50993f6e57ac857b83e0fb67ab7ff
Chase Smith <casmith@solutionreach.com>
2020-05-12 14:13:23 -0600

    adding new project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Chase Smith on 5/12/20.
+//  Copyright © 2020 Chase Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
