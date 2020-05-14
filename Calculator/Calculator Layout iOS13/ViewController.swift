514041eae99506b138718c97564fb220c5d7d119
Chase Smith <casmith@solutionreach.com>
2020-05-14 09:16:34 -0600

    adding AutoLayout and 8 ball projects

---
+++ Calculator/Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Angela Yu on 01/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func percentButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func plusMinusButton(_ sender: UIButton) {
+    }
+    @IBAction func clearButton(_ sender: UIButton) { }
+    @IBAction func divideButton(_ sender: UIButton) { }
+    @IBAction func sevenButton(_ sender: UIButton) { }
+    @IBAction func eightButton(_ sender: UIButton) { }
+    @IBAction func nineButton(_ sender: UIButton) { }
+    @IBAction func multiplyButton(_ sender: UIButton) { }
+    @IBAction func fourButton(_ sender: UIButton) { }
+    @IBAction func fiveButton(_ sender: UIButton) { }
+    @IBAction func sixButton(_ sender: UIButton) { }
+    @IBAction func minusButton(_ sender: UIButton) { }
+    @IBAction func oneButton(_ sender: UIButton) { }
+    @IBAction func twoButton(_ sender: UIButton) { }
+    @IBAction func threeButton(_ sender: UIButton) { }
+    @IBAction func plusButton(_ sender: UIButton) { }
+    @IBAction func zeroButton(_ sender: UIButton) { }
+    @IBAction func decimalButton(_ sender: UIButton) { }
+    @IBAction func equalButton(_ sender: UIButton) { }
+}
+
